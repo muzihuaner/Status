488caaae2b066fa9e79d7cbb0bcdaae1b269fa59
muzihuaner <muzihuaner@hotmail.com>
2023-12-13 20:43:45 +0800

    更新程序

--- config.js
+++ config.js
@@ -1,35 +1,26 @@
-// 配置
 window.Config = {
 
-  // 站点名
+  // 显示标题
   SiteName: '欢哥の站点监控',
 
-  // 站点链接
-  SiteUrl: '/',
-
-  // UptimeRobot Api 域名
-  // 只需填写域名部分，默认为官网域名
-  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
-  // 详见 https://github.com/yb/uptime-status/ 说明
-  // ApiDomain: 'api.uptimerobot.com',
-  //uptimerobot.hgnet.workers.dev
-  ApiDomain: 'api.uptimerobot.com',
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur1057677-4a23cc37dc8ce5c670feb4f2',
   ],
 
-  // 是否显示监测站点的链接
-  ShowLink: false,
-
   // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
+    {
+      text: '欢哥科技',
+      url: 'https://huangetech.github.io'
+    },
     {
       text: '快点搜',
       url: 'https://quickso.cn/'
@@ -38,5 +29,5 @@ window.Config = {
       text: '博客',
       url: 'https://blog.quickso.cn'
     }
-  ]
+  ],
 };
